0907e237a47f6edb5e9b256ec0d0023f18fc4584
Vladislav Korbut <v.korbut8@gmail.com>
2017-08-10 13:34:49 +0300

    creating page fix: deleting item

--- src/containers/questions/edit/Multiple.jsx
+++ src/containers/questions/edit/Multiple.jsx
@@ -4,6 +4,13 @@ import Fa from 'react-fontawesome'
 import { CHECKBOX } from '../../../types/questions'
 
 class Multiple extends Component {
+  componentWillMount() {
+    this.setState({
+      options: this.props.question.options,
+      title: this.props.question.title,
+      isRequired: this.props.question.isRequired,
+    })
+  }
   constructor() {
     super();
     this.state = {
@@ -11,13 +18,6 @@ class Multiple extends Component {
       title: '',
       isRequired: true,
     }
-  }
-  componentWillReceiveProps(newProps) {
-    this.setState({
-      options: newProps.question.options,
-      title: newProps.question.title,
-      isRequired: newProps.question.isRequired,
-    })
   }
   addOption = () => {
     let options = this.state.options;
